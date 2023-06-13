b878ffdfa7cfd910d51698742aade8377c95176b
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2023-06-13 22:35:14 +0530

    Practiced Basic maths logic

---
+++ src/BasicMaths/AllDivsor.java
@@ -0,0 +1,19 @@
+package BasicMaths;
+
+public class AllDivsor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 36;
+		divisor(n);
+	}
+
+	public static void divisor(int n) {
+		System.out.print("The divisor of " + n + " are : ");
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
